42ae7c36d4949c03f118ec0ea14f1656edde262c
John Daniel <jondan97@gmail.com>
2020-02-17 03:12:20 +0200

    added 'to do' list

--- src/main/java/gr/university/thesis/ThesisApplication.java
+++ src/main/java/gr/university/thesis/ThesisApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ThesisApplication {
 
-    //test
     public static void main(String[] args) {
         SpringApplication.run(ThesisApplication.class, args);
     }
