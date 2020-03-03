7bab73d9f17ea22944825d58e76546f7959d9675
jondan97 <jondan97@gmail.com>
2020-03-03 22:06:13 +0200

    hotfix for composite key warning shown about the SprintItemId.java class (needed hashCode and equals method)

--- src/main/java/gr/university/thesis/entity/SprintItemId.java
+++ src/main/java/gr/university/thesis/entity/SprintItemId.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * class that is being used as an @Embeddable type to hold the composite entity identifier of the history repository
@@ -32,5 +33,20 @@ public class SprintItemId implements Serializable {
     @Column
     private Long item_id;
 
-    //maybe an equals is needed to check for both ids (reminder to check later if it works)
+    //maybe an equals is needed to check for both ids (reminder to check later if it works) ----> UPDATE: 1 month later,
+    // yes I needed a hashCode method...
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SprintItemId that = (SprintItemId) o;
+        return Objects.equals(sprint_id, that.sprint_id) &&
+                Objects.equals(item_id, that.item_id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sprint_id, item_id);
+    }
 }
